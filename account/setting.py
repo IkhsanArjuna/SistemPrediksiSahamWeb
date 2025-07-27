576335773fe2daf3e4c58998e2b6056fa95724ec
IkhsanArjuna <134663297+IkhsanArjuna@users.noreply.github.com>
2025-07-27 12:28:51 +0700

    Update setting.py

--- account/setting.py
+++ account/setting.py
@@ -81,7 +81,10 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'account/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (upload gambar)
 MEDIA_URL = '/media/'
